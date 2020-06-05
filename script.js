cb748d12a9cc754f66872a27f66881930eba509d
Natalia <aproken@yandex.ru>
2020-06-05 21:16:01 +0300

    feat: add popup

--- script.js
+++ script.js
@@ -15,5 +15,43 @@ window.addEventListener('DOMContentLoaded', () => {
   clubСhoice();
 
 
+  //Popup
+  const popup = () => {
+    const links = document.querySelectorAll('[data-popup]');
+
+    links.forEach(item => {
+      item.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const itemId = e.currentTarget.dataset.popup;
+        showPopup(itemId);
+
+      })
+    })
+
+    const showPopup = (elemId) => {
+      const elem = document.querySelector(elemId);
+      elem.style.display = 'block';
+    } 
+    
+    const closePopup = () => {
+      const popup = document.querySelectorAll('.popup');
+      popup.forEach(item => item.style.display = 'none');
+    }
+
+    const btnClose = document.querySelectorAll('.close_icon'),
+      overlay = document.querySelectorAll('.overlay');
+    
+    btnClose.forEach( item => item.addEventListener('click', closePopup));
+
+    overlay.forEach( item => item.addEventListener('click', closePopup));
+
+    const btnGift = document.querySelector('.fixed-gift');
+    btnGift.addEventListener('click', () => btnGift.style.display = 'none');
+    
+  }
+
+
+  popup();
 
 });
